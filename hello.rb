7bc7c73e32554bc6d1057871445754159684a65c
a-rom <a1623at@aiit.ac.jp>
2016-09-19 05:57:11 +0000

    名前のディレクトリ追加しました

--- hello.rb
+++ hello.rb
@@ -1,5 +1,20 @@
 require 'sinatra'
 
 get '/' do
- "Hello World!"
+<<EOS
+<html>
+<head>
+ <h1>"Hello World!"</h1>
+</head>
+</html>
+EOS
 end
+
+get '/hello/:name' do
+<<EOS
+<html>
+<head>
+ <h1>"Hello World!,#{params['name']}!"</h1>
+</head>
+EOS
+</html>
